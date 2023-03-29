7baf565b4cc1f889a8e2d5879d0e10ca3fa9387a
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-28 23:48:55 -0300

    Implementação: Cria classe principal para o SpringBoot funcionar

---
+++ src/main/java/com/trybe/acc/java/minhasseries/SeriesAplication.java
@@ -0,0 +1,12 @@
+package com.trybe.acc.java.minhasseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**Classe SeriesAplication.*/
+@SpringBootApplication
+public class SeriesAplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SeriesAplication.class, args);
+  }
+}
