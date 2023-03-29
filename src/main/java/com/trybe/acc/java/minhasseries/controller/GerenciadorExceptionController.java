c95bb889c78b06e72e7bccf4b64ccbb156c1cf11
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-28 23:52:07 -0300

    Implementação: Criação do Controller Advice para configuraçao das exceptions

---
+++ src/main/java/com/trybe/acc/java/minhasseries/controller/GerenciadorExceptionController.java
@@ -0,0 +1,43 @@
+package com.trybe.acc.java.minhasseries.controller;
+
+import com.trybe.acc.java.minhasseries.exception.DataError;
+import com.trybe.acc.java.minhasseries.exception.EpisodioExistenteException;
+import com.trybe.acc.java.minhasseries.exception.ErroInesperadoException;
+import com.trybe.acc.java.minhasseries.exception.SerieExistenteException;
+import com.trybe.acc.java.minhasseries.exception.SerieNaoEncontradaException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**Classe ControllerAdvice. */
+@ControllerAdvice
+public class GerenciadorExceptionController {
+  
+  /**Gerenciador de SerieNaoEncontradaException. */
+  @ExceptionHandler(SerieNaoEncontradaException.class)
+  public ResponseEntity<DataError> notFound(RuntimeException exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new DataError(exception.getMessage()));
+  }
+
+  @ExceptionHandler({
+    SerieExistenteException.class,
+    EpisodioExistenteException.class
+  })
+  public ResponseEntity<DataError> conflict(RuntimeException exception) {
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(new DataError(exception.getMessage()));
+  }
+  
+  @ExceptionHandler(ErroInesperadoException.class)
+  public ResponseEntity<DataError> internalServerError(RuntimeException exception) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(new DataError(exception.getMessage()));
+  }
+  
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<DataError> serviceUnavailable(RuntimeException exception) {
+    return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+        .body(new DataError(exception.getMessage()));
+  }
+  
+}
